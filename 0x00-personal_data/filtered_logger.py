13b9fb00df246342c0fb50e11b87e73b1657c4a3
gouhmada <gouhmada@gmail.com>
2024-02-07 18:31:57 +0000

    return str

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-"""A module for filtering"""
-import os
-import re
-import logging
-# import mysql.connector
+"""A module for filtering the log"""
 from typing import List
+import re
+# import os
+# import logging
+# import mysql.connector
 
 
 def filter_datum(fields: List[str],
-                 redaction: str, message: str, separator: str):
+                 redaction: str, message: str, separator: str) -> str:
     """returns the log message obfuscated"""
     for field in fields:
         pattern = r"{}=([^{}]+)".format(field, separator)
