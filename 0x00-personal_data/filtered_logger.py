a805308240619f8a52ee19447c01301af4bcff98
gouhmada <gouhmada@gmail.com>
2024-02-07 19:30:00 +0000

    ok

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,10 +2,10 @@
 """A module for filtering the log"""
 from typing import List
 import re
-from os import getenv
+import os
 import logging
 import mysql.connector
-from mysql.connector.connection import MySQLConnection
+# from mysql.connector.connection import MySQLConnection
 
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
@@ -31,12 +31,12 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db() -> MySQLConnection:
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """ connector to a database"""
-    username = getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    password = getenv("PERSONAL_DATA_DB_PASSWORD", "")
-    host = getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = getenv("PERSONAL_DATA_DB_NAME", "")
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
     connection = mysql.connector.connect(
         host=host,
         user=username,
