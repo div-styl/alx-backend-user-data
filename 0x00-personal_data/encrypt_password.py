89364314514eaa16ac0456d118bf9ef2c4370369
gouhmada <gouhmada@gmail.com>
2024-02-07 19:36:23 +0000

    fixed encrypting

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,10 +5,10 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     """return hashed password of a user"""
-    slt = bcrypt.genselt()
-    return bcrypt.haspw(password.encode("utf-8"), slt)
+    slt = bcrypt.gensalt()
+    return bcrypt.hashpw(password.encode("utf-8"), slt)
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """function that expects 2 arguments and returns a boolean."""
-    return bcrypt.checkpw(password.encode("utf-8"), hash_password)
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
