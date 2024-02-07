31c86d920140a7e2c4838eba0f01de091e23c45e
gouhmada <gouhmada@gmail.com>
2024-02-07 18:55:09 +0000

    implement format

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,7 +3,7 @@
 from typing import List
 import re
 # import os
-# import logging
+import logging
 # import mysql.connector
 
 
@@ -14,3 +14,28 @@ def filter_datum(fields: List[str],
         pattern = r"{}=([^{}]+)".format(field, separator)
         message = re.sub(pattern, "{}={}".format(field, redaction), message)
     return message
+
+
+def get_logger() -> logging.Logger:
+    """ return the logging logger obj"""
+    pass
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """fomrt the fitered"""
+        notsafe_str = super().format(record)
+        return filter_datum (
+            self.fields, self.REDACTION, notsafe_str, self.SEPARATOR
+        )
