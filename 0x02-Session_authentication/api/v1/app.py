c2d5df6ad14f3a0a7ac3958742a48b6809dad892
gouhmada <gouhmada@gmail.com>
2024-02-16 12:11:37 +0000

    formation

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
+
+import os
 from os import getenv
+from typing import Tuple
+
+from flask import Flask, abort, jsonify, request
+from flask_cors import CORS, cross_origin
+
+from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
 from api.v1.auth.session_db_auth import SessionDBAuth
 from api.v1.auth.session_exp_auth import SessionExpAuth
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
-from flask_cors import CORS, cross_origin
-import os
-from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
@@ -47,25 +51,24 @@ def forbidden(error) -> str:
 @app.before_request
 def before_requests():
     """ handle before rendering """
-    if auth is not None:
-        if auth.require_auth(
-            request.path,
-            [
-                "/api/v1/status/",
-                "/api/v1/unauthorized/",
-                "/api/v1/forbidden/",
-                "/api/v1/auth_session/login/",
-            ],
-        ):
-            if (
-                auth.authorization_header(request) is None
-                and auth.session_cookie(request) is None
-            ):
-                abort(401)
-            elif auth.current_user(request) is None:
-                abort(403)
-            else:
-                request.current_user = auth.current_user(request)
+    if auth is None:
+        return
+    execlued_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
+    ]
+    if not auth.require_auth(request.path, execlued_paths):
+        return
+    auth_hd = auth.authorization_header(request)
+    session_ck = auth.session_cookie(request)
+    if auth_hd is None and session_ck is None:
+        abort(401)
+    user = auth.current_user(request)
+    if user is None:
+        abort(403)
+    request.current_user = user
 
 
 if __name__ == "__main__":
