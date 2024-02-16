673a77dd5641bf73287e00cffc15747a67095686
gouhmada <gouhmada@gmail.com>
2024-02-16 17:18:47 +0000

    app updated

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -19,7 +19,8 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-auth_type = os.getenv('AUTH_TYPE')
+
+auth_type = getenv('AUTH_TYPE', 'default')
 if auth_type == "basic_auth":
     auth = BasicAuth()
 elif auth_type == "session_exp_auth":
@@ -37,13 +38,13 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> str:
+def unauthorized(error: Exception) -> Tuple[jsonify, int]:
     """ Not auth handler """
     return jsonify({'error': 'Unauthorized'}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error) -> str:
+def forbidden(error: Exception) -> Tuple[jsonify, int]:
     """ forbidden handler"""
     return jsonify({'error': 'Forbidden'}), 403
 
@@ -53,13 +54,11 @@ def before_requests():
     """ handle before rendering """
     if auth is None:
         return
-    execlued_paths = [
-        '/api/v1/status/',
-        '/api/v1/unauthorized/',
-        '/api/v1/forbidden/',
-        '/api/v1/auth_session/login/'
-    ]
-    if not auth.require_auth(request.path, execlued_paths):
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
+    if not auth.require_auth(request.path, excluded_paths):
         return
     auth_hd = auth.authorization_header(request)
     session_ck = auth.session_cookie(request)
@@ -74,4 +73,4 @@ def before_requests():
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, debug=True)
